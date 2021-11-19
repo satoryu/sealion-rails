0e98a3698339aa0f4f4fb693206e47f56920bd51
Tatsuya Sato <satoryu.1981@gmail.com>
2021-11-19 15:42:14 +0000

    Add a test to confirm existing todos are listed

--- Gemfile
+++ Gemfile
@@ -67,6 +67,7 @@ group :test do
   gem "capybara", ">= 3.26"
   gem "selenium-webdriver"
   gem "webdrivers"
+  gem 'rails-controller-testing'
 end
 
 
--- spec/requests/todos_spec.rb
+++ spec/requests/todos_spec.rb
@@ -9,5 +9,17 @@ RSpec.describe 'Todos', type: :request do
       expect(response).to render_template(:index)
       expect(assigns(:todos)).to be_empty
     end
+
+    context 'When some todos have been already registered' do
+      let(:todos) { create_list :todo, 2 }
+
+      it 'returns http success' do
+        get '/todos'
+
+        expect(response).to have_http_status(:success)
+        expect(response).to render_template(:index)
+        expect(assigns(:todos)).to eql(todos)
+      end
+    end
   end
 end
