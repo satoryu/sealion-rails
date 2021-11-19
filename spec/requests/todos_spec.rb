3a015fc7e501f5eef424aad3def1e489ef1a78eb
Tatsuya Sato <satoryu.1981@gmail.com>
2021-11-19 15:31:10 +0000

    Todo#index should assign todo as empty

--- spec/requests/todos_spec.rb
+++ spec/requests/todos_spec.rb
@@ -1,11 +1,13 @@
 require 'rails_helper'
 
-RSpec.describe "Todos", type: :request do
-  describe "GET /index" do
+RSpec.describe 'Todos', type: :request do
+  describe 'GET /' do
     it "returns http success" do
-      get "/todos/index"
+      get '/todos'
+
       expect(response).to have_http_status(:success)
+      expect(response).to render_template(:index)
+      expect(assigns(:todos)).to be_empty
     end
   end
-
 end
