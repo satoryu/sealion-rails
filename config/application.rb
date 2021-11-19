a04f164dd62227bf432f0ed6490fa3e3c42b0476
Tatsuya Sato <satoryu.1981@gmail.com>
2021-11-19 15:12:20 +0000

    Disable generating helper specs and routing specs

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,12 @@ module Sealion
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
 
+    config.generators do |generator|
+      generator.controller = { skip_routes: true }
+      generator.helper = false
+      generator.test_framework :rspec, helper_specs: false, routing_specs: false
+    end
+
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
