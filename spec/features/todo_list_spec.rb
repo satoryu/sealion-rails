460c70f21cbc8fdc32298535d98e38450143a73e
Tatsuya Sato <satoryu.1981@gmail.com>
2021-11-19 17:07:25 +0000

    Implement a view template to list all todos

--- spec/features/todo_list_spec.rb
+++ spec/features/todo_list_spec.rb
@@ -1,7 +1,12 @@
 require 'rails_helper'
 
 RSpec.feature "TodoLists", type: :feature do
-  scenario '' do
+  before do
+    create :todo, content: 'Buy a milk'
+    create :todo, content: 'Call back the office'
+  end
+
+  scenario 'Show all registered todos' do
     visit '/todos'
 
     expect(page).to have_text('Buy a milk')
