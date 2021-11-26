34a113782ca2c12873daa9c02396b58bbd21a155
Tatsuya Sato <satoryu.1981@gmail.com>
2021-11-26 13:46:45 +0000

    Use turbo_frame_tag

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -1,11 +1,14 @@
 class TodosController < ApplicationController
+  before_action :load_todo_list, only: %i[index create]
+  before_action :load_new_todo, only: %i[index create]
+
   def index
-    @todo = Todo.new
-    @todos = Todo.all
   end
 
   def create
     Todo.create(todo_params)
+
+    return render :index if turbo_frame_request?
 
     redirect_to todos_path
   end
@@ -14,5 +17,13 @@ class TodosController < ApplicationController
 
   def todo_params
     params.require(:todo).permit(:content)
+  end
+
+  def load_todo_list
+    @todos = Todo.all
+  end
+
+  def load_new_todo
+    @todo = Todo.new
   end
 end
