7b2f58b0f60a8a99c84095bfd9bec9562f3c6927
Tatsuya Sato <satoryu.1981@gmail.com>
2021-11-26 13:24:33 +0000

    Fix a spec

--- spec/features/create_todo_spec.rb
+++ spec/features/create_todo_spec.rb
@@ -5,7 +5,7 @@ RSpec.feature "CreateTodos", type: :feature do
     visit '/todos'
 
     fill_in 'Content', with: 'Buy a New Gudget'
-    click_button 'Submit'
+    click_button 'Create Todo'
 
     expect(page).to have_text('Buy a New Gudget')
   end
