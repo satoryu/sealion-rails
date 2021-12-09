62a1768c9c9da2f9a645c1afbf22e12a210cc105
Tatsuya Sato <satoryu.1981@gmail.com>
2021-12-09 06:21:23 +0000

    todo list does not show completed todos

--- spec/features/todo_list_spec.rb
+++ spec/features/todo_list_spec.rb
@@ -4,6 +4,7 @@ RSpec.feature "TodoLists", type: :feature do
   before do
     create :todo, content: 'Buy a milk'
     create :todo, content: 'Call back the office'
+    create :todo, content: 'Go to the Office' # Completed one
   end
 
   scenario 'Show all registered todos' do
@@ -11,5 +12,6 @@ RSpec.feature "TodoLists", type: :feature do
 
     expect(page).to have_text('Buy a milk')
     expect(page).to have_text('Call back the office')
+    expect(page).to have_no_text('Go to the Office')
   end
 end
