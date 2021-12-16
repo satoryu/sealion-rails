0bacf623ca6b32dd4b1d13639f6ce724d1692202
Tatsuya Sato <satoryu.1981@gmail.com>
2021-12-16 03:56:25 +0000

    Define a scope to fetch completed Todos

--- app/models/todo.rb
+++ app/models/todo.rb
@@ -1,3 +1,5 @@
 class Todo < ApplicationRecord
   validates :content, presence: true
+
+  scope :completed, -> { where.not(completed_at: nil) }
 end
--- spec/models/todo_spec.rb
+++ spec/models/todo_spec.rb
@@ -1,5 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe Todo, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe '.completed' do
+    let!(:completed_todo) { create :todo, content: 'Bar', completed_at: Time.zone.now }
+
+    before do
+      create :todo, content: 'Foo'
+    end
+
+    it 'fetches only incomplete todos' do
+      todos = Todo.completed
+
+      expect(todos).to contain_exactly(completed_todo)
+    end
+  end
 end
