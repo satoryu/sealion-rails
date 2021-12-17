c01ed97cec21ae3d2c021fae22fcea9ca017d2dd
Tatsuya Sato <satoryu.1981@gmail.com>
2021-12-17 06:02:12 +0000

    Add a link to incomplete todos page on completed todo page

--- spec/features/show_completed_todos_spec.rb
+++ spec/features/show_completed_todos_spec.rb
@@ -14,4 +14,22 @@ RSpec.feature "ShowCompletedTodos", type: :feature do
       expect(page).to have_text('Go to the Office')
     end
   end
+
+  context 'And visits Incomplete TODOs' do
+    background do
+      click_link 'Incomplete Todos'
+    end
+
+    scenario 'shows incomplete TODOs' do
+      within 'ul' do
+        expect(page).to have_text('Buy a Milk')
+        expect(page).to have_no_text('Go to the Office')
+      end
+    end
+    scenario 'shows the form to register new Todo' do
+      within 'form' do
+        expect(page).to have_field('Content')
+      end
+    end
+  end
 end
