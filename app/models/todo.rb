36e1446d9264257273c9667e11eb7bb63af66e19
Tatsuya Sato <satoryu.1981@gmail.com>
2021-12-19 21:45:39 +0000

    Use imcomplete's where_values_hash to define completed todo

--- app/models/todo.rb
+++ app/models/todo.rb
@@ -2,7 +2,7 @@ class Todo < ApplicationRecord
   validates :content, presence: true
 
   scope :incomplete, -> { where(completed_at: nil) }
-  scope :completed, -> { where.not(completed_at: nil) }
+  scope :completed, -> { where.not(incomplete.where_values_hash) }
 
   def completed?
     completed_at.present?
