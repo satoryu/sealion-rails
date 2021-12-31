265a2867b04544402b547ccda2198285bf0ff9cb
Tatsuya Sato <satoryu.1981@gmail.com>
2021-12-31 14:49:30 +0000

    Create new user record if first sign in

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -2,6 +2,17 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
   skip_before_action :verify_authenticity_token, only: :github
 
   def github
-    redirect_to todos_path
+    @user = User.create(provider: auth_hash.provider, uid: auth_hash.uid, email: auth_hash.info.email, password: Devise.friendly_token[0, 20])
+
+    sign_in_and_redirect @user
+  end
+
+  private
+  def auth_hash
+    request.env['omniauth.auth']
+  end
+
+  def after_sign_in_path_for(user)
+    todos_path
   end
 end
--- spec/requests/users/omniauth_callbacks_spec.rb
+++ spec/requests/users/omniauth_callbacks_spec.rb
@@ -2,11 +2,20 @@ require 'rails_helper'
 
 RSpec.describe "Users::OmniauthCallbacks", type: :request do
   describe "GET /github" do
-    it "returns http success" do
+    before do
+      Rails.application.env_config['devise.mapping'] = Devise.mappings[:user]
+      Rails.application.env_config['omniauth.auth'] = OmniAuth.config.mock_auth[:github]
+    end
+
+    it "returns redirects to todos page" do
       get "/users/auth/github/callback"
 
       expect(response).to redirect_to(todos_path)
     end
+    it "creates user record" do
+      expect {
+        get "/users/auth/github/callback"
+      }.to change(User, :count).by(1)
+    end
   end
-
 end
